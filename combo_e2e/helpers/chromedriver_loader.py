492f793d768882ecd9389078effaf1507ecc06a1
samuelfirst <a.a.sheykin@gmail.com>
2025-02-04 13:08:13 +0300

    Use new API for downloading chromedriver version upper 115

--- combo_e2e/helpers/chromedriver_loader.py
+++ combo_e2e/helpers/chromedriver_loader.py
@@ -44,9 +44,11 @@ class ChromeDriverLoader:
     """
 
     driver_name: str = "chromedriver"
+    zip_driver_path: str = "chromedriver-{platform}"
     _path_to_store: Path = None
     path_to_download: Path = None
     driver_path: str = None
+    version: str = ""
 
     @classmethod
     def download(cls, path_to_download: Path, driver_path: Path) -> None:
@@ -62,7 +64,7 @@ class ChromeDriverLoader:
         logger.info(" Starting download chrome driver")
         version = cls._get_latest_version()
         archive_path = cls._download(version)
-        cls.driver_path = cls._unzip(archive_path)
+        cls.driver_path = cls._unzip(archive_path, version=version)
         logger.info(
             "Prepare tests stage. Chrome driver downloaded and saved in %s",
             cls.driver_path,
@@ -98,14 +100,31 @@ class ChromeDriverLoader:
     def _get_latest_version(cls):
         version = config.DEFAULT_DRIVER_VER
         if not config.DEFAULT_DRIVER_VER:
-            res = cls._get(urljoin(config.CHROME_DRIVER_URL, config.CHROME_DRIVER_VER))
-            version = res.content.decode("utf-8").strip()
+            res = cls._get(config.CHROME_DRIVER_LAST_KNOWN_URL)
+            try:
+                versions = res.json()
+                version = versions.get("channels", {}).get("Stable", {}).get("version")
+            except Exception as e:
+                raise ChromeDriverLoaderException(
+                    f"Cannot get chromedriver version. Url: {config.CHROME_DRIVER_LAST_KNOWN_URL}. "
+                    f"Response: {res.text}"
+                )
         return version
 
     @classmethod
+    def _is_old_version(cls, version: str) -> bool:
+        major_version = int(version.split(".")[0])
+        return major_version < config.NEW_CHROME_DRIVER_VER
+
+    @classmethod
     def _download(cls, version: str) -> Path:
-        file_relative_path = str(Path(version).joinpath(config.CHROME_DRIVER_FILE_NAME))
-        download_url = urljoin(config.CHROME_DRIVER_URL, file_relative_path)
+        if cls._is_old_version(version):
+            file_relative_path = str(Path(version).joinpath(config.CHROME_DRIVER_FILE_NAME))
+            download_url = urljoin(config.CHROME_DRIVER_URL, file_relative_path)
+        else:
+            file_name = config.NEW_CHROME_DRIVER_FILE_NAME.format(platform=config.CHROME_DRIVER_PLATFORM)
+            chrome_driver_url = config.NEW_CHROME_DRIVER_URL.format(version=version, platform=config.CHROME_DRIVER_PLATFORM)
+            download_url = urljoin(chrome_driver_url, file_name)
         res = cls._get(download_url)
         return cls._save_zip_to_tmp(res.content)
 
@@ -117,13 +136,20 @@ class ChromeDriverLoader:
         return path_to_write
 
     @classmethod
-    def _unzip(cls, archive_path: Path) -> str:
+    def _unzip(cls, archive_path: Path, version: str) -> str:
         archive = zipfile.ZipFile(file=archive_path)
         driver_file_path = cls.make_driver_full_path()
         if driver_file_path.exists():
             logger.info("Remove previouse driver at: %s", driver_file_path)
             driver_file_path.unlink()
-        archive.extract(cls.driver_name, path=str(cls._path_to_store))
+
+        if cls._is_old_version(version):
+            archive.extract(member=cls.driver_name, path=str(cls._path_to_store))
+        else:
+            zip_driver_file_path = f"{cls.zip_driver_path.format(platform=config.CHROME_DRIVER_PLATFORM)}/{cls.driver_name}"
+            with open(cls._path_to_store.joinpath(cls.driver_name), "wb") as f:
+                f.write(archive.read(zip_driver_file_path))
+
         archive.close()
         if not driver_file_path.exists():
             raise ChromeDriverLoaderException(
